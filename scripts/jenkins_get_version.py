6c40ea91b884cebd217a7e59b6341269dd96d92b
Maciek Kokot <mk@b-sharp.pl>
2021-06-20 14:50:29 +0200

    versioning script modification

--- scripts/jenkins_get_version.py
+++ scripts/jenkins_get_version.py
@@ -1,10 +1,13 @@
 import jenkins
 
 server = jenkins.Jenkins('http://localhost:8080',
-                         username='admin',
-                         password='')  # place password here
+                         username='',  # implement env vars in future
+                         password='')  # implement env vars in future
 
 
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+plugins = server.get_plugins_info()
+print(plugins)
